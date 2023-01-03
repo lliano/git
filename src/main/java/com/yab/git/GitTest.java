a2ea164b87a566c792e09cf74fd8a1e5c1c2677d
linyabin <yab.lin@sunyard.com>
2023-01-03 17:21:56 +0800

    hot fix 冲突 commit

--- src/main/java/com/yab/git/GitTest.java
+++ src/main/java/com/yab/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello git333!");
         System.out.println("hello git333!");
         System.out.println("hello git333!");
+        //合并冲突hotfix
         //hot fix 冲突合并
         //hot fox 分支不冲突合并
     }
