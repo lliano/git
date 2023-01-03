6e08c34088bca10a0f96426f247a39fee4d61b0d
linyabin <yab.lin@sunyard.com>
2023-01-03 17:07:53 +0800

    second commit

--- src/main/java/com/yab/git/GitTest.java
+++ src/main/java/com/yab/git/GitTest.java
@@ -6,5 +6,7 @@ package com.yab.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
